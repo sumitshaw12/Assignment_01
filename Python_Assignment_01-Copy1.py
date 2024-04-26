1b1df8551aca107a87c5e6d2b99aa0bd35b90f0c
sumitshaw12 <154542641+sumitshaw12@users.noreply.github.com>
2024-04-26 21:20:43 +0530

    Add files via upload

---
+++ Python_Assignment_01-Copy1.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Assignment-01 
+# 
+
+# 1. In the below elements which of them are values or an expression? eg:- values can be
+# integer or string and expressions will be mathematical operators.
+# *
+# 'hello'
+# -87.8
+# -
+# /
+# +
+# 6
+# 
+# Ans:- values are 'hello',-87.8,6
+#       expressions are *,-,/,+
+
+# 2. What is the difference between string and variable?
+# 
+# Ans:-Variables are symbols that we can use to store data in a program. We can think of them as an empty box that you fill with some data or value. Strings are data, so we can use them to fill up a variable. Declaring strings as variables can make it easier for us to work with strings throughout our Python programs. A String is usually words, enclosed with "" Eg String x ="Welcome to SoloLearn" X is the Variable, and we declared it as a String, use the single = to assign the text to it.
+
+# 3. Describe three different data types.
+# 
+# ans:-Integers, floating-point numbers, and strings.
+# 
+
+# 4. What is an expression made up of? What do all expressions do?
+# 
+# ans:-An expression is a construct made up of variables, operators, and method invocations, which are constructed according to the syntax of the language, that evaluates to a single value.
+# Expression in python can be used in a variety of ways, such as in assignment statements, function arguments, and conditional statements.
+
+# 5. This assignment statements, like spam = 10. What is the difference between an
+# expression and a statement?
+# 
+# ans:-A statement is a complete line of code that performs some action, while an expression is any section of the code that evaluates to a value.
+
+# 6. After running the following code, what does the variable bacon contain?
+# bacon = 22
+# bacon + 1
+# 
+# ans:- bacon contain 22.
+
+# 7. What should the values of the following two terms be?
+# 'spam' + 'spamspam'
+# 'spam' * 3
+# 
+# ans:-'spamspamspam'
+#      'spamspamspam'
+
+# 8. Why is eggs a valid variable name while 100 is invalid?
+# 
+# ans:-Variable names cannot begin with a number but it can start with any alphabet.
+
+# 9. What three functions can be used to get the integer, floating-point number, or string
+# version of a value?
+# 
+# ans:-The int(), float(), and str() functions will evaluate to the integer, floating-point number, and string versions of the value passed to them.
+
+# 10. Why does this expression cause an error? How can you fix it?
+# 'I have eaten' + 99 + 'burritos.'
+# 
+# Ans:-The expression causes an error because 99 is an integer, and only strings can be concatenated to other strings with the + operator. The correct way is I have eaten ' + str(99) + ' burritos.'.
